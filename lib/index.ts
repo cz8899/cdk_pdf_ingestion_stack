4a780f92c515fe0e6e006bba9c3fca49bf7c7126
cz8899 <chriszheng9@gmail.com>
2025-06-27 21:39:01 -0400

    Update index.ts
    fix tsc missing error

--- lib/index.ts
+++ lib/index.ts
@@ -3,35 +3,37 @@ import { Construct } from 'constructs';
 import * as s3 from 'aws-cdk-lib/aws-s3';
 import * as opensearch from 'aws-cdk-lib/aws-opensearchserverless';
 import * as bedrock from 'aws-cdk-lib/aws-bedrock';
+import * as iam from 'aws-cdk-lib/aws-iam';
 
-export class DevGeniusStack extends cdk.Stack {
+export class DevGeniusMiniStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // Retrieve Bedrock Agent role ARN from CDK context
-    const bedrockRoleArn = this.node.tryGetContext('bedrockRoleArn');
-    if (!bedrockRoleArn) {
-      throw new Error('bedrockRoleArn must be provided via CDK context');
-    }
-
-    // Create S3 bucket for PDFs (no policies attached)
+    // S3 Bucket for PDFs
     const pdfBucket = new s3.Bucket(this, "KnowledgeBasePDFs", {
       bucketName: `${cdk.Stack.of(this).stackName}-kb-pdfs`,
       autoDeleteObjects: true,
       removalPolicy: cdk.RemovalPolicy.DESTROY,
-      encryption: s3.BucketEncryption.S3_MANAGED,
-      enforceSSL: true,
-      blockPublicAccess: s3.BlockPublicAccess.BLOCK_ACLS
+      encryption: s3.BucketEncryption.S3_MANAGED
     });
 
-    // Create OpenSearch Serverless collection
+    // OpenSearch Serverless (AOSS) Collection
     const collection = new opensearch.CfnCollection(this, "VectorStore", {
       name: "devgenius-kb",
       type: "VECTORSEARCH"
     });
 
+    // IAM Role for Bedrock (manually assigned later)
+    const bedrockRoleArn = this.formatArn({
+      service: "iam",
+      account: cdk.Stack.of(this).account,
+      region: "us-west-2",
+      resource: "role/BedrockAgentRole",
+      arnFormat: cdk.ArnFormat.STANDARD
+    });
+
     // Bedrock Knowledge Base (requires manual IAM role assignment)
-    new bedrock.CfnKnowledgeBase(this, "AWSWellArchitectedKB", {
+    const knowledgeBase = new bedrock.CfnKnowledgeBase(this, "AWSWellArchitectedKB", {
       name: "aws-well-architected-kb",
       roleArn: bedrockRoleArn,
       knowledgeBaseConfiguration: {
@@ -60,5 +62,8 @@ export class DevGeniusStack extends cdk.Stack {
         }
       }
     });
+
+    // Ensure AOSS collection is ready before Knowledge Base
+    knowledgeBase.node.addDependency(collection);
   }
 }
