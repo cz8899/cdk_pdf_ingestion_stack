e313c7317f33b2488a01aab9d377de3dd243ad64
cz8899 <chriszheng9@gmail.com>
2025-06-28 15:11:03 -0400

    Delete app.py
    no need

--- app.py
+++
@@ -1,6 +0,0 @@
-import aws_cdk as cdk
-from cdk_pdf_ingestion_stack.cdk_pdf_ingestion_stack_stack import CdkPdfIngestionStack
-
-app = cdk.App()
-CdkPdfIngestionStack(app, "CdkPdfIngestionStack")
-app.synth()
